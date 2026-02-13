e8836af6302c5ade9c901383a4cb47a174582882
jeremy-di <93599332+jeremy-di@users.noreply.github.com>
2026-02-13 10:39:37 +0100

    Update archiveActivity.controller.js

--- controllers/archiveActivity.controller.js
+++ controllers/archiveActivity.controller.js
@@ -23,39 +23,4 @@ const getArchiveActivityById = async(req,res) => {
     }
 }
 
-// const updateArchiveActivity = async(req,res) => {
-//     try {
-//         const {body} = req
-//         if(!body){
-//             return res.status(400).json({message: "No data in the request"})
-//         }
-
-//         const {error} = archiveActivityValidation(body).archiveActivityUpdate
-//         if(error){
-//             return res.status(401).json(error.details[0].message)
-//         }
-//         const updatedArchiveActivity = await ArchiveActivity.findByIdAndUpdate(req.params.id, body, {new: true})
-//         if(!updatedArchiveActivity){
-//             res.status(404).json({message: "archiveActivity doesn't exist"})
-//         }
-//         return res.status(200).json(updatedArchiveActivity)
-//     } catch (error) {
-//         console.log(error)
-//         res.status(500).json({message: "Server error", error: error})
-//     }
-// }
-
-// const deleteArchiveActivity = async(req, res) => {
-//     try {
-//         const archiveActivity = await ArchiveActivity.findByIdAndDelete(req.params.id)
-//         if(!archiveActivity){
-//             return res.status(404).json({message: "archiveActivity doesn't exist"})
-//         }
-//         return res.status(200).json({message: "archiveActivity has been deleted"})
-//     } catch (error) {
-//         console.log(error)
-//         res.status(500).json({message: "Server error", error: error})
-//     }
-// }
-
 export { getAllArchiveActivitys, getArchiveActivityById }
